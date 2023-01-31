cffb715dff457cb788791e0693f552e6ef55580b
ricosar <122638957+ricosar@users.noreply.github.com>
2023-01-31 10:00:14 -0500

    Changes to print info
    updating it with my own information!

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -53,19 +53,19 @@ public class CardTrick {
         System.out.println("Congratulations, you guessed right!");
         System.out.println();
         
-        System.out.println("My name is Paul, but you can call me prof, Paul or sir");
+        System.out.println("My name is Sarah!");
         System.out.println();
         
         System.out.println("My career ambitions:");
-        System.out.println("-- Be more active on LinkedIn");
-        System.out.println("-- Have a semester with no violations of academic integrity!");
+        System.out.println("-- Being a software developer working in a company!");
+        System.out.println("-- Doing well in all my semesters at Sheridan :)");
 	System.out.println();	
 
         System.out.println("My hobbies:");
-        System.out.println("-- Investing");
-        System.out.println("-- Cooking");
-        System.out.println("-- Reading/Watching TV");
-        System.out.println("-- Riding my motorcycle");
+        System.out.println("-- Drawing and doing 3d artwork!");
+        System.out.println("-- Hanging out with my friends!");
+        System.out.println("-- Coding in my spare time!);
+        System.out.println("-- Playing video games!");
 
         System.out.println();
         
